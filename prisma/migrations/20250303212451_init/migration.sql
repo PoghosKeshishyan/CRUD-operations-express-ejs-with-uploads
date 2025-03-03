971283ed96eff6596cc5d3bf5f1f2bff2a390c1e
Poghos <119891530+PoghosKeshishyan@users.noreply.github.com>
2025-03-04 01:43:17 +0400

    Add files via upload

---
+++ prisma/migrations/20250303212451_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "image" TEXT NOT NULL
+);
